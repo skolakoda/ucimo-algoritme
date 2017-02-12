9a1be1694105f87f049f5af40db0239573c06102
Damjan Pavlica <mudroljub@gmail.com>
2017-02-12 13:24:13 +0100

    redjanje umetanjem

---
+++ sortiranje/redjanje-umetanjem.js
@@ -0,0 +1,21 @@
+const slucajniNiz = require('../utils/slucajniNiz')
+
+const poredjajUmetanjem = niz => {
+  let pocniOd = 1
+  while (pocniOd < niz.length) {
+    for (let i = 0; i < pocniOd; i++) {
+      const nesvrstan = niz[pocniOd]
+      if (nesvrstan < niz[i]) {
+        niz.splice(pocniOd, 1)
+        niz.splice(i, 0, nesvrstan)
+      }
+    }
+    pocniOd ++
+  }
+  return niz
+}
+
+const niz = slucajniNiz(20)
+console.log(niz)
+const poredjanNiz = poredjajUmetanjem(niz)
+console.log(poredjanNiz)
