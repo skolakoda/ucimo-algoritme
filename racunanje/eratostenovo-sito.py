16453f07136e0d99d0da7509fff40cba4306dc99
Damjan Pavlica <mudroljub@gmail.com>
2018-04-15 14:10:47 +0200

    novi algoritam

---
+++ racunanje/eratostenovo-sito.py
@@ -0,0 +1,14 @@
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+def find_primes(n):
+  primes = []
+  composites = []
+
+  for i in range (2, n+1):
+    if i not in composites:
+      primes.append(i)
+      for j in range(i*i, n+1, i):
+        composites.append(j)
+  return primes
+
+print(find_primes(100))
