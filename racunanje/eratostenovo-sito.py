f188dae476c71fe7f23ce7bcc06493c6b98e913d
Damjan Pavlica <mudroljub@gmail.com>
2018-04-15 14:11:33 +0200

    novi algoritam

--- racunanje/eratostenovo-sito.py
+++ racunanje/eratostenovo-sito.py
@@ -2,13 +2,13 @@
 
 def find_primes(n):
   primes = []
-  composites = []
+  composites = set()
 
   for i in range (2, n+1):
     if i not in composites:
       primes.append(i)
       for j in range(i*i, n+1, i):
-        composites.append(j)
+        composites.add(j)
   return primes
 
 print(find_primes(100))
