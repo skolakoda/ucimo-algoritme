154ec788d5c16c98c5a1129112e6da05381eec61
Damjan Pavlica <mudroljub@gmail.com>
2018-12-21 22:20:09 +0100

    game of life m

--- 90-primeri/game-of-life/main.js
+++ 90-primeri/game-of-life/main.js
@@ -1,60 +1,48 @@
-/**
- * Conway's Game of Life
- * http://jsfiddle.net/ankr/tgjLA/
- */
-const canvas = document.getElementById('c').getContext('2d')
-canvas.strokeStyle = '#e1e1e1'
-canvas.fillStyle = 'cadetblue'
+const ctx = document.getElementById('c').getContext('2d')
+ctx.fillStyle = '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
 const ceilSize = 8
 const gridSize = ceilSize * ceilSize // 64
 const cWidth = ceilSize * gridSize // 512
 const cHeight = ceilSize * gridSize // 512
 
-function draw(cells) {
-  canvas.clearRect(0, 0, cWidth, cHeight)
-  cells.forEach((row, x) => {
-    row.forEach((cell, y) => {
-      canvas.beginPath()
-      canvas.rect(x * ceilSize, y * ceilSize, ceilSize, ceilSize)
-      canvas[cell ? 'fill' : 'stroke']()
-    })
-  })
+function draw(matrix) {
+  ctx.clearRect(0, 0, cWidth, cHeight)
+  matrix.forEach((row, x) => row.forEach((cell, y) => {
+    ctx.beginPath()
+    ctx.rect(x * ceilSize, y * ceilSize, ceilSize, ceilSize)
+    if (cell) ctx.fill()
+  }))
 }
 
-function countNeighbours(x, y, cells) {
+function countNeighbours(x, y, matrix) {
   let neighbours = 0
-  const exists = (x, y) => cells[x] && cells[x][y]
   for (let h = -1; h <= +1; h++) {
     for (let v = -1; v <= +1; v++) {
       if (h == 0 && v == 0) continue
-      if (exists(x + h, y + v)) neighbours++
+      if (matrix[x + h] && matrix[x + h][y + v]) neighbours++
     }
   }
   return neighbours
 }
 
-function nextGeneration(cells) {
-  return cells.map((row, x) => row.map((cell, y) => {
-    const neighbours = countNeighbours(x, y, cells)
+function updateMatrix(matrix) {
+  return matrix.map((row, x) => row.map((cell, y) => {
+    const neighbours = countNeighbours(x, y, matrix)
     return neighbours === 3 || (cell && neighbours === 2)
   }))
 }
 
-function update(cells) {
-  const newCells = nextGeneration(cells)
-  draw(newCells)
-  setTimeout(() => update(newCells), 70)
+function update(matrix) {
+  const newMatrix = updateMatrix(matrix)
+  draw(newMatrix)
+  setTimeout(() => update(newMatrix), 70)
 }
 
 function init() {
-  const cells = []
-  for (let i = 0; i < gridSize; i++) {
-    cells[i] = []
-    for (let j = 0; j < gridSize; j++) {
-      cells[i][j] = Math.random() >= 0.8
-    }
-  }
-  update(cells)
+  const matrix = Array(gridSize).fill().map(() =>
+    Array(gridSize).fill().map(() => Math.random() >= 0.8)
+  )
+  update(matrix)
 }
 
 init()
