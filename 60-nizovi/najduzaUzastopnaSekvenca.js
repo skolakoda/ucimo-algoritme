24fc25ca943d8b96c8d8532e200b4340fb542f89
Damjan Pavlica <mudroljub@gmail.com>
2024-12-05 12:23:57 +0100

    m

--- 60-nizovi/najduzaUzastopnaSekvenca.js
+++ 60-nizovi/najduzaUzastopnaSekvenca.js
@@ -2,18 +2,18 @@ function najduzaUzastopnaSekvenca(arr) {
   if (arr.length === 0) return 0
 
   let maxDuzinaSekvence = 1 
-  let trenutnaDuzinaSekvence = 1 
+  let trenutnaDuzina = 1 
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] === arr[i - 1] + 1) {
-      trenutnaDuzinaSekvence++
+      trenutnaDuzina++
     } else {
-      maxDuzinaSekvence = Math.max(maxDuzinaSekvence, trenutnaDuzinaSekvence)
-      trenutnaDuzinaSekvence = 1
+      maxDuzinaSekvence = Math.max(maxDuzinaSekvence, trenutnaDuzina)
+      trenutnaDuzina = 1
     }
   }
 
-  return Math.max(maxDuzinaSekvence, trenutnaDuzinaSekvence)
+  return Math.max(maxDuzinaSekvence, trenutnaDuzina)
 }
 
 const brojevi = [1, 2, 3, 5, 6, 7, 8, 10]
